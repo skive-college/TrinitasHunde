9ee2854341bbb4323e901b3fb96265bb4b2613dc
Kenneth Lund Kyrler <kenn3933@skivecollege.dk>
2020-01-28 10:51:41 +0100

    Basic SqlConnection, SqlCommand & constructor with connectionString

--- TrinitasDataAccess/Database.cs
+++ TrinitasDataAccess/Database.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace TrinitasDataAccess
 {
     public class Database
     {
+        private readonly string connectionString;
+        private SqlConnection connection;
+        private SqlCommand command;
+
+        public Database(string _connectionString)
+        {
+            connectionString = _connectionString;
+        }
     }
 }
