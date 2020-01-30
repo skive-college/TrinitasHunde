1a3fb3b2f5c83ddc3490c3be567e8f9cb9115260
Kenneth Lund Kyrler <kenn3933@skivecollege.dk>
2020-01-30 09:17:54 +0100

    Rip maps marker

--- TrinitasHunde/Pages/Public/Map.cshtml.cs
+++ TrinitasHunde/Pages/Public/Map.cshtml.cs
@@ -10,9 +10,47 @@ namespace TrinitasHunde.Pages.Public
 
     public class MapModel : PageModel
     {
+        public List<object> Objects { get; set; }
+        public List<Pin> Pins { get; set; }
+
+        public MapModel()
+        {
+            TrinitasDataAccess.Database database = new TrinitasDataAccess.Database(
+               @"Data Source=planner.aspitweb.dk;Initial Catalog=trinitashunde;user id = aspitlab; password = aspitlab; Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            Objects = database.getAllObjects();
+
+            Pins = new List<Pin>();
+            //Converts List of anonymous objects to Pin type object
+            foreach (var item in Objects)
+            {
+                Type type = item.GetType();
+                Pin pin = new Pin()
+                {
+                    Name = (string)type.GetProperty("Name").GetValue(item),
+                    PinType = (string)type.GetProperty("PinType").GetValue(item),
+                    LocationType = (string)type.GetProperty("Type").GetValue(item),
+                    GPSLatitude = (double)type.GetProperty("GPSLatitude").GetValue(item),
+                    GPSLongitude = (double)type.GetProperty("GPSLongitude").GetValue(item)
+                };
+                if (pin.PinType == "Godkendt")
+                {
+                    pin.PinColor = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
+                }
+                else if (pin.PinType == "Afventer")
+                {
+                    pin.PinColor = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
+                }
+                else if (pin.PinType == "Afvist")
+                {
+                    pin.PinColor = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+                }
+                Pins.Add(pin);
+            }
+        }
+
         public void OnGet()
         {
-
+           
         }
     }
 }
---
+++ TrinitasHunde/Pin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrinitasHunde
+{
+    public class Pin
+    {
+        public string Name { get; set; }
+        public double GPSLatitude { get; set; }
+        public double GPSLongitude { get; set; }
+        public string PinColor { get; set; }
+        public string PinType { get; set; }
+        public string LocationType { get; set; }
+    }
+}
