20d50cb596d8a3fb773ff43b47e9c2c01d65b8a1
Kenneth Lund Kyrler <kenn3933@skivecollege.dk>
2020-01-30 10:17:24 +0100

    Pins on Maps fixed

--- TrinitasHunde/Pages/Public/Map.cshtml.cs
+++ TrinitasHunde/Pages/Public/Map.cshtml.cs
@@ -18,20 +18,22 @@ namespace TrinitasHunde.Pages.Public
             TrinitasDataAccess.Database database = new TrinitasDataAccess.Database(
                @"Data Source=planner.aspitweb.dk;Initial Catalog=trinitashunde;user id = aspitlab; password = aspitlab; Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             Objects = database.getAllObjects();
-
             Pins = new List<Pin>();
-            //Converts List of anonymous objects to Pin type object
+
+            //Converts each object in a List of anonymous objects to Pin type object
             foreach (var item in Objects)
             {
                 Type type = item.GetType();
                 Pin pin = new Pin()
                 {
+                    //System reflection to get property values from anonymous object type
                     Name = (string)type.GetProperty("Name").GetValue(item),
                     PinType = (string)type.GetProperty("PinType").GetValue(item),
                     LocationType = (string)type.GetProperty("Type").GetValue(item),
                     GPSLatitude = (double)type.GetProperty("GPSLatitude").GetValue(item),
                     GPSLongitude = (double)type.GetProperty("GPSLongitude").GetValue(item)
                 };
+                //Maps marker layouts > https://sites.google.com/site/gmapsdevelopment/
                 if (pin.PinType == "Godkendt")
                 {
                     pin.PinColor = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
