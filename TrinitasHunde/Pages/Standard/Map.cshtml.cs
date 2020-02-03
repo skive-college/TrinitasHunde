bcac8269c7b40413a820bb9a3473d37d47908524
tokeob <58528260+tokeob@users.noreply.github.com>
2020-02-03 12:44:22 +0100

    Fixed namespaces

--- TrinitasHunde/Pages/Standard/Map.cshtml.cs
+++ TrinitasHunde/Pages/Standard/Map.cshtml.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using TrinitasDataAccess;
 
-namespace TrinitasHunde.Pages.Admin
+namespace TrinitasHunde.Pages.Standard
 {
     public class MapModel : PageModel
     {
