237f22acbfdaa4f8cd2ef01d1a7dfb05236c6953
tokeob <58528260+tokeob@users.noreply.github.com>
2020-02-04 13:14:07 +0100

    Finished Admin/Users page

--- TrinitasHunde/Pages/Admin/Users.cshtml.cs
+++ TrinitasHunde/Pages/Admin/Users.cshtml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Threading.Tasks;
 using TrinitasDataAccess;
 using TrinitasHunde.Data;
 using TrinitasHunde.Models;
@@ -15,12 +16,16 @@ namespace TrinitasHunde.Pages.Admin
     public class UsersModel : PageModel
     {
         private readonly ApplicationDbContext dbcontext;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public List<ApplicationUser> PendingUsers { get; set; }
+        public IList<ApplicationUser> PendingUsers { get; set; }
+        public IList<ApplicationUser> AdminUsers { get; set; }
+        public IList<ApplicationUser> StandardUsers { get; set; }
 
-        public UsersModel(ApplicationDbContext dbcontext)
+        public UsersModel(ApplicationDbContext dbcontext, UserManager<ApplicationUser> userManager)
         {
             this.dbcontext = dbcontext;
+            this.userManager = userManager;
         }
 
         public void OnGet()
@@ -31,11 +36,47 @@ namespace TrinitasHunde.Pages.Admin
             PendingUsers = dbcontext.Users.Where(user =>
                 !usersWithRoles.Contains(user.Id)
             ).ToList();
+
+            AdminUsers = userManager.GetUsersInRoleAsync("Admin").Result;
+            StandardUsers = userManager.GetUsersInRoleAsync("Standard").Result;
         }
 
-        public void OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
-            ;
+            switch (Request.Form["submit"].ToString().ToLowerInvariant())
+            {
+                case "accept":
+                    await acceptUser(Request.Form["email"]);
+                    break;
+                case "decline":
+                    await declineUser(Request.Form["email"]);
+                    break;
+                default:
+                    // Invalid post, do nothing
+                    break;
+            }
+            // Redirect to Get for PRG
+            return RedirectToPage();
+        }
+
+        private async Task acceptUser(string email)
+        {
+            // Add user to Standard role
+            ApplicationUser user = await userManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                await userManager.AddToRoleAsync(user, "Standard");
+            }
+        }
+
+        private async Task declineUser(string email)
+        {
+            // Delete user
+            ApplicationUser user = await userManager.FindByEmailAsync(email);
+            if(user != null)
+            {
+                await userManager.DeleteAsync(user);
+            }
         }
     }
 }
