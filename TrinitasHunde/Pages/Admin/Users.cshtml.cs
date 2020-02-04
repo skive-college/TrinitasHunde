61cdc98283b1e9c4c321441b0ab7df2326eb274f
tokeob <58528260+tokeob@users.noreply.github.com>
2020-02-04 11:56:13 +0100

    Created unfinished Admin/Users page

---
+++ TrinitasHunde/Pages/Admin/Users.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using TrinitasDataAccess;
+using TrinitasHunde.Data;
+using TrinitasHunde.Models;
+
+namespace TrinitasHunde.Pages.Admin
+{
+    public class UsersModel : PageModel
+    {
+        private readonly ApplicationDbContext dbcontext;
+
+        public List<ApplicationUser> PendingUsers { get; set; }
+
+        public UsersModel(ApplicationDbContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        public void OnGet()
+        {
+            // Get users with roles
+            IQueryable<string> usersWithRoles = dbcontext.UserRoles.Select(userRole => userRole.UserId).Distinct();
+            // Get users without any role
+            PendingUsers = dbcontext.Users.Where(user =>
+                !usersWithRoles.Contains(user.Id)
+            ).ToList();
+        }
+
+        public void OnPost()
+        {
+            ;
+        }
+    }
+}
